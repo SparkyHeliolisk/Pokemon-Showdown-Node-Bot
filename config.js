36e76cad421dd37aa9cf94c4ad092f92aba26d2a
SparkyHeliolisk <SparkyHeliolisk@users.noreply.github.com>
2019-04-18 19:09:46 +0100

    Update config.js

--- config.js
+++ config.js
@@ -46,7 +46,7 @@ exports.watchconfig = true;
 
 exports.nick = 'pikpikpik';
 
-exports.pass = '*';
+exports.pass = '';
 
 exports.autoReloginDelay = 60 * 1000;
 
@@ -106,7 +106,7 @@ exports.globalPermissions = {
 * Commands configuration
 */
 
-exports.commandTokens = ['*']; //Array of valid command characters
+exports.commandTokens = ['+']; //Array of valid command characters
 
 exports.defaultPermission = '%';
 
